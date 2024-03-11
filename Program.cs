e60194a5ef9e3a5714947041de02add9a98ce5a4
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-03-11 15:30:44 -0300

    (feat): adicionando método que registra as bandas

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// Screen Sound
+
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound"; 
 
 void ExibirLogo()
@@ -22,7 +23,7 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
-    Console.WriteLine("Digite -1 para sair");
+    Console.WriteLine("Digite 5 para sair");
 
     Console.Write("\nDigite a sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
@@ -30,19 +31,40 @@ void ExibirOpcoesDoMenu()
     
     switch (opcaoEscolhidaNumerica)
     {
-        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-            break;
-        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-            break;
-        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-            break;
-        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
-            break;
-        case -1: Console.WriteLine("Tchau tchau :)");
-            break;
-        default: Console.WriteLine("Opção inválida");
-            break;
+        case 1:
+            RegistrarBanda();
+        break;
+        case 2: 
+            Console.WriteLine($"Você escolheu a opção {opcaoEscolhidaNumerica}");
+        break;
+        case 3: 
+            Console.WriteLine($"Você escolheu a opção {opcaoEscolhidaNumerica}");
+        break;
+        case 4:
+            Console.WriteLine($"Você escolheu a opção {opcaoEscolhidaNumerica}");
+        break;
+        case 5: 
+            Console.WriteLine("Tchau tchau :)");
+        break;
+        default: 
+            Console.WriteLine("Opção inválida");
+        break;
     }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Bandas");
+    Console.Write("Por favor, digite o nome da banda que deseja registrar: ");
+
+    string nomeDaBanda = Console.ReadLine()!;
+
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    
+    Console.Clear();
+    ExibirOpcoesDoMenu();
 }
 
 ExibirOpcoesDoMenu();
