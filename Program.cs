f6c71aa1e0e51886277b5634fb50dcda1cf758e6
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2024-03-13 09:12:18 -0300

    (feat): adiciona função que avalia as bandas

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ void ExibirOpcoesDoMenu()
             MostrarBandasRegistradas();
         break;
         case 3: 
-            Console.WriteLine($"Você escolheu a opção {opcaoEscolhidaNumerica}");
+            AvaliarUmaBanda();
         break;
         case 4:
             Console.WriteLine($"Você escolheu a opção {opcaoEscolhidaNumerica}");
@@ -96,6 +96,34 @@ void ExibirTituloDaOpcao(string titulo)
     Console.WriteLine(asteriscos);
     Console.WriteLine(titulo);
     Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+
+        Console.WriteLine($"\nA nota {nota} para a banda {nomeDaBanda} foi registrada com sucesso!");
+        Thread.Sleep(4000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
 }
 
 ExibirOpcoesDoMenu();
